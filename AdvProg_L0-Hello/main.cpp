d7111098527e73714e591449a67bbcf72578b01e
thankhoibg <24020185@vnu.edu.vn>
2025-03-21 14:36:40 +0700

    complete guessit

--- AdvProg_L0-Hello/main.cpp
+++ AdvProg_L0-Hello/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "hello.h"
 
+// why this code can not run ?
+// because the function printGameOver() is not defined in hello.h
+// so the compiler can not find the definition of the function
+
 int main(){
     std::cout << printGameOver() << std::endl ;
 }
